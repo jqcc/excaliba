286d16e5d3ab137a6bb092363c90f8cca0f74aa9
Qiang <897238475@qq.com>
2021-02-25 16:15:34 +0800

    delete useless data

--- datas/sample/t.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import codecs
-
-with open("test.txt", "rb") as f:
-    with codecs.open("test_.txt", "w", encoding="utf-8") as w:
-        d = pickle.load(f)
-        for tr, tx in zip(d[0], d[1]):
-            w.write(" ".join([str(x) for x in tr]) + " " + str(tx) + "\n")
